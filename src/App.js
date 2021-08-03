86087c429f884a5d81d21e06e8057b5776080340
Gaurangkothiya <gaurangkothiya1234@gmail.com>
2021-08-03 12:26:54 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="card" id='first'>
+      <div className="card-body">
+        Hello guys
+      </div>
+    </div>
+  );
+}
+
+export default App;
